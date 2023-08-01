d310ea4ba0f5a10f4ab3ae71b244eb0dc7969984
Матвей Шевченко <shevchenko9542@gmail.com>
2023-08-01 18:21:33 +0600

    Added token filter

--- api-gateway/src/main/java/com/rn/apigateway/config/RoutingConfig.java
+++ api-gateway/src/main/java/com/rn/apigateway/config/RoutingConfig.java
@@ -8,11 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RoutingConfig {
-    public static final String AUTH_PATH = "/api/auth";
-    public static final String USER_SERVICE_PATH = "/api/user";
-    public static final String USER_SERVICE_URI = "http://user-service:8080";
-    public static final String SSE_SERVICE_PATH = "/api/sse";
-    public static final String SSE_SERVICE_URI = "http://sse-service:8081";
 
 
 
@@ -27,15 +22,16 @@ public class RoutingConfig {
     public RouteLocator routing(RouteLocatorBuilder builder) {
         return builder.routes()
             .route(r -> r
-                .path(AUTH_PATH + "/signup")
-                .filters(f -> f.setPath(USER_SERVICE_PATH + "/create"))
-                .uri(USER_SERVICE_URI))
+                .path("/res")
+                .filters(f -> f.setPath("/res1"))
+                .uri("http://localhost:8001"))
+            /*
             .route(r -> r
-                .path(AUTH_PATH + "/verify-email**")
+                .path("AUTH_PATH" + "/verify-email**")
                 .filters(f -> f
                     .setPath(USER_SERVICE_PATH + "/verify-email")
                     .filter(signUpFilter))
-                .uri(USER_SERVICE_URI))
+                .uri(USER_SERVICE_URI))*/
             .build();
     }
 
--- api-gateway/src/main/java/com/rn/apigateway/filter/AuthFilter.java
+++ api-gateway/src/main/java/com/rn/apigateway/filter/AuthFilter.java
@@ -1,30 +1,45 @@
 package com.rn.apigateway.filter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component
-public class AuthFilter implements GlobalFilter {
-    String apiTokenCookieName;
-    String apiTokenCookiePath;
-    long apiTokenExpirationMs;
-    long apiTokenCookieExpirationS;
-    String authTokenCookieName;
-    String authTokenCookiePath;
-    long authTokenExpirationMs;
-    long authTokenCookieExpirationS;
-    JwtParser authTokenParserBuilder;
-    JwtParser apiTokenParserBuilder;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Logger;
 
+@Component
+public class AuthFilter implements Ordered, GlobalFilter {
+    private final String apiTokenCookieName;
+    private final String apiTokenCookiePath;
+    private final long apiTokenExpirationMs;
+    private final long apiTokenCookieExpirationS;
+    private final String authTokenCookieName;
+    private final String authTokenCookiePath;
+    private final long authTokenExpirationMs;
+    private final long authTokenCookieExpirationS;
+    private final JwtParser authTokenParser;
+    private final JwtParser apiTokenParser;
+    private final JwtBuilder authTokenBuilder;
+    private final JwtBuilder apiTokenBuilder;
 
 
 
@@ -52,17 +67,25 @@ public class AuthFilter implements GlobalFilter {
         this.authTokenExpirationMs = authTokenExpirationMs;
         this.authTokenCookieExpirationS = authTokenCookieExpirationS;
 
-        apiTokenParserBuilder = Jwts.parserBuilder()
+        apiTokenParser = Jwts.parserBuilder()
             .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(apiTokenSecretKey)))
             .requireIssuer(apiTokenIssuer)
             .setAllowedClockSkewSeconds(5)
             .build();
 
-        authTokenParserBuilder = Jwts.parserBuilder()
+        authTokenParser = Jwts.parserBuilder()
             .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(authTokenSecretKey)))
             .requireIssuer(authTokenIssuer)
             .setAllowedClockSkewSeconds(5)
             .build();
+
+        apiTokenBuilder = Jwts.builder()
+            .setIssuer(apiTokenIssuer)
+            .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(apiTokenSecretKey)), SignatureAlgorithm.HS256);
+
+        authTokenBuilder = Jwts.builder()
+            .setIssuer(authTokenIssuer)
+            .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(authTokenSecretKey)), SignatureAlgorithm.HS256);
     }
 
 
@@ -70,9 +93,48 @@ public class AuthFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ///
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            ///
-        }));
+        ServerHttpRequest request = exchange.getRequest().mutate().headers((httpHeaders) -> {
+            String userID = "service-guest";
+            String[] roles = new String[]{"ROLE_GUEST"};
+            HttpCookie authTokenCookie = exchange.getRequest().getCookies().getFirst(authTokenCookieName);
+
+            if (authTokenCookie != null) {
+                String authToken = authTokenCookie.getValue();
+
+                try {
+                    Claims claims = authTokenParser.parseClaimsJws(authToken).getBody();
+                    userID = claims.getSubject();
+                    roles = (String[]) claims.get("roles");
+                } catch (
+                    SignatureException |
+                    MalformedJwtException |
+                    ExpiredJwtException |
+                    UnsupportedJwtException |
+                    IllegalArgumentException e
+                ) {
+                    ///
+                    System.out.println("Invalid token");
+                    ///
+                }
+            }
+
+            HashMap<String,Object> rolesClaims = new HashMap<>();
+            rolesClaims.put("roles", roles);
+
+            String apiTokenCookie = new HttpCookie(apiTokenCookieName, apiTokenBuilder
+                .setSubject(userID)
+                .addClaims(rolesClaims)
+                .setExpiration(new Date(System.currentTimeMillis() + apiTokenExpirationMs))
+                .compact()).toString();
+
+            httpHeaders.set("Cookie", apiTokenCookie);
+        }).build();
+
+        return chain.filter(exchange.mutate().request(request).build());
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
